939b88286850e2d917c5219437297206c3fcbd16
SilverSoul <z1079931505@gmail.com>
2023-03-30 01:40:43 -0400

    updated test script for 2d evaluation

--- tests/test_tsgp.py
+++ tests/test_tsgp.py
@@ -5,7 +5,7 @@ import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 os.sys.path.insert(0, parentdir)
-from models.thompson_sampling import ThompsonSamplingGP 
+from models.thompson_sampling import ThompsonSampling, MultiTaskThompsonSampling
 
 # importing necessary modules
 import numpy as np
@@ -14,6 +14,8 @@ from matplotlib.animation import FuncAnimation
 from IPython.display import HTML
 from tqdm import tqdm
 from time import time
+
+import argparse
 
 # turning off automatic plot showing, and setting style
 plt.ioff()
@@ -31,6 +33,9 @@ def ackley_1d(x, y=0):
     # returning
     return out
 
+def ackley_2d(X):
+    out = ackley_1d(X[0], X[1])
+    return out
 
 # function to create an animation with the visualization
 def ts_gp_animation_ackley(ts_gp, max_rounds, X, Y, output_dir=None):
@@ -43,39 +48,23 @@ def ts_gp_animation_ackley(ts_gp, max_rounds, X, Y, output_dir=None):
     for round_id in range(max_rounds):
         
         # recording all the info
-        X_observed, y_observed, X_grid, posterior_sample, posterior_mean, posterior_std = ts_gp.choose_next_sample()
+        X_observed, y_observed = ts_gp.choose_next_sample()
 
-        
         # adding to dict
-        round_dict[round_id] = {'X_observed': X_observed.numpy(), 
-                                'y_observed': y_observed.numpy(), 
-                                'X_grid': X_grid.numpy(),
-                                'posterior_sample': posterior_sample.numpy(),
-                                'posterior_mean': posterior_mean.numpy(),
-                                'posterior_std': posterior_std.numpy()}
+        round_dict[round_id] = {'X_observed': X_observed.numpy()[:, 0], 
+                                'y_observed': y_observed.numpy()
+                                }
     print(f"Time Elapsed: {time()-start_time:.4f} s")
+    xval, fval = ts_gp.get_result()
+    print(f"Found minimum objective {fval:.4f} at {xval}")
     # plotting!
     fig, ax = plt.subplots(figsize=[10,4], dpi=150)
-    
-    # plotting first iteration
-    ax.plot(X, Y, 'k--', linewidth=2, label='Actual function')
-    ax.plot(round_dict[0]['X_observed'], round_dict[0]['y_observed'], 'bo', label="""GP-Chosen Samples of Ackley's function""", alpha=0.7)
-    ax.plot(round_dict[0]['X_grid'], round_dict[0]['posterior_sample'], 'r', linewidth=2, label='Sample from the posterior', alpha=0.7)
-    ax.fill_between(round_dict[0]['X_grid'], round_dict[0]['posterior_mean'] - round_dict[0]['posterior_std'],
-                     round_dict[0]['posterior_mean'] + round_dict[0]['posterior_std'], alpha=0.2, color='r')
-    plt.title("""Ackley's function at $y=0$, TS-GP optimization""", fontsize=14) 
-    plt.xlabel('$x$') 
-    plt.ylabel('$f(x)$') 
-    ax.set_ylim(-0.5, 10)
-        
+
     # function for updating
     def animate(i):
         ax.clear()
         ax.plot(X, Y, 'k--', linewidth=2, label='Actual function')
         ax.plot(round_dict[i]['X_observed'], round_dict[i]['y_observed'], 'bo', label="""GP-Chosen Samples of Ackley's function""", alpha=0.7)
-        ax.plot(round_dict[i]['X_grid'], round_dict[i]['posterior_sample'], 'r', linewidth=2, label='Sample from the posterior', alpha=0.7)
-        ax.fill_between(round_dict[i]['X_grid'], round_dict[i]['posterior_mean'] - round_dict[i]['posterior_std'],
-                         round_dict[i]['posterior_mean'] + round_dict[i]['posterior_std'], alpha=0.2, color='r')
         plt.title("""Ackley's function at $y=0$, TS-GP optimization""", fontsize=14) 
         plt.xlabel('$x$') 
         plt.ylabel('$f(x)$')
@@ -83,7 +72,7 @@ def ts_gp_animation_ackley(ts_gp, max_rounds, X, Y, output_dir=None):
         return ()
 
     # function for creating animation
-    anim = FuncAnimation(fig, animate, frames=max_rounds, interval=2000, blit=True)
+    anim = FuncAnimation(fig, animate, frames=max_rounds, interval=500, blit=True)
     if output_dir is not None:
         anim.save(f"{output_dir}/test_tsgp.mp4")
     else:
@@ -91,14 +80,26 @@ def ts_gp_animation_ackley(ts_gp, max_rounds, X, Y, output_dir=None):
         return HTML(anim.to_html5_video())
 
 if __name__ == "__main__":
-    # instance of our TS-GP
-    ts_gp = ThompsonSamplingGP(n_random_draws=2, objective=ackley_1d, x_bounds=(-4,4))
-    # suppressing warnings, gp can complain at times
-    import warnings 
-    warnings.filterwarnings("ignore")
-    
-    # data
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dim", type=int, default=1, required=True, choices=[1, 2])
+    parser.add_argument("--step", type=int, default=50, required=False)
+    args = parser.parse_args()
+
+    # ground truth data for 1d ackley
     X = np.linspace(-4, 4, 500)
     Y = ackley_1d(X)
+
+    print(f"Testing bayesian optimization on {args.dim}-D Ackley function...")
+    if not args.dim==2:
+        bounds = np.array([-4, 4])
+        objective = ackley_1d
+    else:
+        bounds = np.array([[-4, 4], [-4, 4]])
+        objective = ackley_2d
+
+    # instance of multi-task TS-GP
+    multi_ts_gp = MultiTaskThompsonSampling(n_random_draws=5, objective=objective, bounds=bounds)
+    
     # showing animnation
-    ts_gp_animation_ackley(ts_gp, 20, X, Y, "figures/")
+    ts_gp_animation_ackley(multi_ts_gp, args.step, X, Y, "figures/")
+    print("The result video has been saved under figures folder")
