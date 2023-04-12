2b0fde69c805e2b7eaf8493416ee5372b020b943
SilverSoul <z1079931505@gmail.com>
2023-04-12 19:22:46 -0400

    add partial running cost

--- controller/pushing_controller.py
+++ controller/pushing_controller.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from controller.mppi import MPPI
+from functools import partial
 
 from env.panda_pushing_env import TARGET_POSE_FREE_BOX, TARGET_POSE_OBSTACLES_BOX, OBSTACLE_CENTRE_BOX, OBSTACLE_HALFDIMS, BOX_SIZE
 
@@ -77,10 +78,14 @@ class PushingController(object):
 
     def set_parameters(self, hyperparameters):
         # ---
+        self.mppi.running_cost = partial(obstacle_avoidance_pushing_cost_function, target_pose=self.target_state)
         self.mppi.set_parameters(hyperparameters)
 
     def get_cost_total(self):
         return self.mppi.get_cost_total()
+    
+    def reset(self):
+        return self.mppi.reset()
 
 
 def free_pushing_cost_function(state, action):
@@ -140,14 +145,14 @@ def collision_detection(state):
     return in_collision.float()
 
 
-def obstacle_avoidance_pushing_cost_function(state, action):
+def obstacle_avoidance_pushing_cost_function(state, action, target_pose):
     """
     Compute the state cost for MPPI on a setup with obstacles.
     :param state: torch tensor of shape (B, state_size)
     :param action: torch tensor of shape (B, state_size)
     :return: cost: torch tensor of shape (B,) containing the costs for each of the provided states
     """
-    target_pose = TARGET_POSE_OBSTACLES_TENSOR.to(dtype=state.dtype, device=state.device)  # torch tensor of shape (3,) containing (pose_x, pose_y, pose_theta)
+    target_pose = target_pose.to(dtype=state.dtype, device=state.device)  # torch tensor of shape (3,) containing (pose_x, pose_y, pose_theta)
     cost = None
     # --- Your code here
     x = state
