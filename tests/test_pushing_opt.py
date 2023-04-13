d87e36761ce4919630fa2c32dfcb1803fa17778b
SilverSoul <z1079931505@gmail.com>
2023-04-13 12:39:21 -0400

    add baynb

--- tests/test_pushing_opt.py
+++ tests/test_pushing_opt.py
@@ -1,6 +1,7 @@
 # working directory
 import os
 import inspect
+from matplotlib import pyplot as plt
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -11,13 +12,12 @@ from tqdm import tqdm
 import numpy as np
 import cma
 import argparse
-
 from model.state_dynamics_models import ResidualDynamicsModel
 from controller.pushing_controller import PushingController, obstacle_avoidance_pushing_cost_function, free_pushing_cost_function
 from optimizer.bayesian_optimization import BayesianOptimization
 from env.panda_pushing_env import PandaBoxPushingEnv, TARGET_POSE_OBSTACLES_BOX, BOX_SIZE, TARGET_POSE_FREE_BOX
 
-def get_total_cost(end_state, target_state, n_steps, k = 0.1, n_collision = 0):
+def get_total_cost(end_state, target_state, n_steps, k = 0.1):
 
     goal_distance = np.linalg.norm(end_state[:2]-target_state[:2]) # evaluate only position, not orientation
     goal_reached = goal_distance > BOX_SIZE
@@ -27,15 +27,14 @@ def get_total_cost(end_state, target_state, n_steps, k = 0.1, n_collision = 0):
 
 def target_state_reset():
     
-    return np.random.uniform(low=[0.05, -0.35, 0.0], high=[.8, 0.35, 0.0], size=None)
-
+    return np.random.uniform(low=[0.5, -0.35, 0.0], high=[0.8, 0.35, 0.0], size=None)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--step", type=int, default=20)
-    parser.add_argument("--epoch", type=int, default=5)
-    parser.add_argument("--optimizer", type=str, default="bayes", choices=["bayes", "cma"])
+    parser.add_argument("--epoch", type=int, default=100)
+    parser.add_argument("--optimizer", type=str, default="bayes", choices=["bayes", "cma", "baynb", "eval"])
     parser.add_argument("--render", action="store_true")
     parser.add_argument("--cuda", action="store_true")
 
@@ -98,11 +97,14 @@ if __name__ == "__main__":
         optimizer = BayesianOptimization(torch.tensor([0, 0, 0, 0]), torch.tensor([1, 10, 10, 10]), acq_mode="ei",device=device)
 
         for _ in range(args.epoch):
+            # target_state = target_state_reset()
             start_state = env.reset()
             state = start_state
 
             parameters = optimizer.suggest()
             controller.set_parameters(parameters)
+            # env.target_state = target_state
+            # controller.set_target_state(target_state)
 
             for i in tqdm(range(args.step)):
                 # parameters = optimizer.suggest()
@@ -125,6 +127,59 @@ if __name__ == "__main__":
         
         xval, fval = optimizer.get_result()
         print(f"Found minimum objective {fval:.4f} at {xval}")
+    elif args.optimizer == "baynb":
+        from bayes_opt import BayesianOptimization, UtilityFunction
+        optimizer = BayesianOptimization(f=None, pbounds={"lambda": (0., 1), 
+                                                          "sigma1": (1e-7, 10.),
+                                                          "sigma2": (1e-7, 10.),
+                                                          "sigma3": (1e-7, 10.)})
+        utility = UtilityFunction(kind="ei", kappa=2.5, xi=0.0)
+        cost_list = []
+        cost_avg_list = []
+        cost_total = 0.
+        for _ in range(args.epoch):
+            # target_state = target_state_reset()
+            start_state = env.reset()
+            state = start_state
+
+            params = optimizer.suggest(utility)
+            parameters = list(params.values())
+            # print(parameters)
+            controller.set_parameters(parameters)
+            # env.target_state = target_state
+            # controller.set_target_state(target_state)
+
+            for i in tqdm(range(args.step)):
+                action = controller.control(state)
+                state, reward, done, _ = env.step(action)
+                if done:
+                    break
+            
+            controller.reset()
+            # Evaluate if goal is reached
+            end_state = env.get_state()
+            goal_distance = np.linalg.norm(end_state[:2]-target_state[:2]) # evaluate only position, not orientation
+            goal_reached = goal_distance < BOX_SIZE
+
+            cost = get_total_cost(end_state, target_state, i)
+            optimizer.register(params=params, target=cost)
+
+            cost_total += cost
+            cost_avg = cost_total/(len(cost_list)+1)
+            cost_list.append(cost)
+            cost_avg_list.append(cost_avg)
+            print(f"AVG COST: {cost_avg}")
+            print(f'COST: {cost}')
+            print(f'GOAL REACHED: {goal_reached}')
+        
+        plt.plot(np.arange(len(cost_list)), cost_list)
+        plt.show()
+        plt.plot(np.arange(len(cost_avg_list)), cost_avg_list)
+        plt.show()
+        print(optimizer.max)
+    else:
+        # TODO evaluation
+        pass
 
     if args.render:
         env.disconnect()
