4239a45de98d679e94f1229b579aab71f0a8d1a3
SilverSoul <z1079931505@gmail.com>
2023-04-14 02:21:19 -0400

    add ideas

---
+++ demo.py
@@ -0,0 +1,7 @@
+# Ideas
+# 1) Run evaluation for pushing task with parameters optimized by bayesian optimization
+#       print confirmation info, print metrics, plot metrics, save to figures
+# 2) Run eval with two baseline parametersn
+#       print confirmation info, print metrics, plot metrics, save to figures
+# * pushing task: which environment with which obstacle setup
+# Note: print out an expected time to run before starting
